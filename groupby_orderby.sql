bdfecd9b764892176532199ee8ed2037aea3bbbe
Rohith Chandra <rohithchandra12345@gmail.com>
2026-06-12 00:21:32 +0530

    Group By and Agrregations

---
+++ groupby_orderby.sql
@@ -0,0 +1,25 @@
+/*
+SELECT department, COUNT(*) AS employee_count 
+FROM employees 
+GROUP BY department;
+
+SELECT department,AVG(salary) AS avg_salary
+FROM employees
+GROUP BY department;
+
+SELECT department,MAX(salary) AS max_salary
+FROM employees
+GROUP BY department;
+
+SELECT department,MIN(salary) AS min_salary
+FROM employees
+GROUP BY department;
+
+SELECT department,SUM(salary) AS total_salary
+FROM employees
+GROUP BY department;
+
+SELECT *FROM employees
+ORDER BY age ASC;
+*/
+
