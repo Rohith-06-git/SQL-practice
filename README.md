d3a00c3c55df2e6d1b285007c4eac6430f115994
Rohith Chandra <rohithchandra12345@gmail.com>
2026-06-12 01:16:11 +0530

    Practised joins

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# SQL-practice
+# SQL Practice
+
+This repository contains my SQL practice queries and exercises as part of my journey toward Data Engineering and AI Engineering.
+
+## Topics Covered
+
+- SELECT
+- WHERE
+- ORDER BY
+- GROUP BY
+- Aggregate Functions
+- INNER JOIN
+- LEFT JOIN
+
+## Purpose
+
+The goal of this repository is to strengthen SQL fundamentals through hands-on practice and problem solving.
+
+## Tech Used
+
+- SQL
+- SQLite
+
+Total Questions Solved : 20+
+
+## Author
+Rohith Chandra
---
+++ joins.sql
@@ -0,0 +1,32 @@
+/*
+SELECT *
+FROM employees e
+INNER JOIN departments d
+ON e.department_id = d.department_id;
+
+SELECT 
+    employees.id,
+    employees.name, 
+    employees.salary, 
+    departments.department_name, 
+    departments.building_floor
+FROM employees
+INNER JOIN departments 
+ON employees.department_id = departments.department_id;
+
+SELECT *
+FROM employees e
+LEFT JOIN departments d
+ON e.department_id = d.department_id;
+
+SELECT COUNT(*)
+FROM employees e
+INNER JOIN departments d
+ON e.department_id = d.department_id;
+
+SELECT d.department_name,COUNT(*)
+FROM employees e
+INNER JOIN departments d
+ON e.department_id = d.department_id
+GROUP BY d.department_name ;
+*/
