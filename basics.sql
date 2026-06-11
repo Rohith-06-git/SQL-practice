d6e8936f554172163a654336f8b1bf1a2fc63a22
Rohith Chandra <rohithchandra12345@gmail.com>
2026-06-11 23:52:46 +0530

    Basics of SQL

---
+++ basics.sql
@@ -0,0 +1,26 @@
+/*
+SELECT * FROM students;
+
+
+SELECT name FROM students;
+
+SELECT * FROM students
+WHERE cgpa > 8;
+
+
+SELECT * FROM students
+WHERE name = 'Rohith';
+
+SELECT * FROM students
+ORDER BY cgpa DESC;
+
+SELECT MIN(cgpa) FROM students ;
+
+SELECT MAX(cgpa) FROM students;
+
+SELECT SUM(cgpa) FROM students ;
+
+SELECT AVG(cgpa) FROM students;
+*/
+SELECT DISTINCT cgpa FROM students;
+
