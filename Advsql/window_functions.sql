8998de83e89d7740aa283d86b612f614e5efaec7
Rohith Chandra <rohithchandra12345@gmail.com>
2026-06-25 15:09:19 +0530

    Completed window functions upto Intermediate Level

--- Advsql/window_functions.sql
+++ Advsql/window_functions.sql
@@ -37,7 +37,7 @@ SELECT * FROM
     ( SELECT name,
          salary ,
          department_id,
-         RANK() OVER (
+         DENSE_RANK() OVER (
              PARTITION BY department_id
              ORDER BY salary DESC
             ) AS rn
@@ -50,12 +50,65 @@ SELECT * FROM (
     SELECT  name,
             salary,
             department_id,
-            RANK() OVER(
+            DENSE_RANK() OVER(
                 PARTITION BY department_id
                 ORDER BY salary DESC
                 )AS rn 
     FROM employees 
     ) AS t 
 WHERE rn < 4 ;
+
+-- Q.Find second-highest salary in each department.
+
+SELECT * FROM (
+    SELECT  name,
+            salary,
+            department_id,
+            DENSE_RANK() OVER(
+                PARTITION BY department_id
+                ORDER BY salary DESC
+            ) AS rn
+    FROM employees 
+    ) AS t
+WHERE rn = 2 ;
+
+-- Q.Find employees whose salary rank is ≤ 5.
+
+SELECT * FROM (
+    SELECT  name,
+            salary,
+            department_id,
+            DENSE_RANK() OVER(
+                PARTITION BY department_id
+                ORDER BY salary DESC
+            ) AS rn
+    FROM employees 
+    ) AS t
+WHERE rn <= 5 ;
+
+-- Q.Find duplicate records using ROW_NUMBER().
+
+SELECT * FROM (
+    SELECT name,
+            id,
+            salary,
+            ROW_NUMBER() OVER(
+                PARTITION BY salary
+                ORDER BY id ASC
+                ) AS rn
+    FROM employees 
+        ) AS t 
+WHERE rn > 1 ;
+
+-- Q.Find nth highest salary using DENSE_RANK().
+
+ SELECT  name,
+         salary,
+         DENSE_RANK() OVER(
+            ORDER BY salary DESC
+        )AS rn
+ FROM employees  ;
+
+Q. Find department-wise highest salary employee.
 
 
