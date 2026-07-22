152beb63c4e01253c973059b9e1fecc4dc0997cd
Rohith Chandra <rohithchandra12345@gmail.com>
2026-07-23 00:11:06 +0530

    Completed upto 15 questions

--- Advsql/practice_questions.sql
+++ Advsql/practice_questions.sql
@@ -1,224 +1,224 @@
--- -- Q. Q1. Find customers who have never placed an order.
--- SELECT c.name
--- FROM customers c
--- LEFT JOIN orders o
--- ON c.customer_id = o.customer_id
--- WHERE o.customer_id IS NULL;
+-- Q. Q1. Find customers who have never placed an order.
+SELECT c.name
+FROM customers c
+LEFT JOIN orders o
+ON c.customer_id = o.customer_id
+WHERE o.customer_id IS NULL;
 
--- -- ALTERNATE 
--- SELECT name
--- FROM customers
--- WHERE customer_id NOT IN (
---     SELECT customer_id
---     FROM orders
--- );
+-- ALTERNATE 
+SELECT name
+FROM customers
+WHERE customer_id NOT IN (
+    SELECT customer_id
+    FROM orders
+);
 
--- -- Q2. Find the second highest salary.
--- WITH salary_rank AS (
---     SELECT salary,
---            DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
---     FROM employees
--- )
--- SELECT salary
--- FROM salary_rank
--- WHERE rnk = 2;
+-- Q2. Find the second highest salary.
+WITH salary_rank AS (
+    SELECT salary,
+           DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+    FROM employees
+)
+SELECT salary
+FROM salary_rank
+WHERE rnk = 2;
 
--- -- ALTERNATE
--- SELECT name,
---        salary
--- FROM employees
--- WHERE salary = (
---     SELECT MAX(salary)
---     FROM employees
---     WHERE salary < (
---         SELECT MAX(salary)
---         FROM employees
---     )
--- );
+-- ALTERNATE
+SELECT name,
+       salary
+FROM employees
+WHERE salary = (
+    SELECT MAX(salary)
+    FROM employees
+    WHERE salary < (
+        SELECT MAX(salary)
+        FROM employees
+    )
+);
 
--- --ALTERNATE 
--- SELECT DISTINCT salary
--- FROM employees
--- ORDER BY salary DESC
--- LIMIT 1 OFFSET 1;
+--ALTERNATE 
+SELECT DISTINCT salary
+FROM employees
+ORDER BY salary DESC
+LIMIT 1 OFFSET 1;
 
--- -- Q3. Employees earning more than their department average.
+-- Q3. Employees earning more than their department average.
 
--- -- Correlated Subquery
--- SELECT name,
---        salary
--- FROM employees e
--- WHERE salary > (
---     SELECT AVG(salary)
---     FROM employees e2
---     WHERE e.department_id = e2.department_id
--- );
+-- Correlated Subquery
+SELECT name,
+       salary
+FROM employees e
+WHERE salary > (
+    SELECT AVG(salary)
+    FROM employees e2
+    WHERE e.department_id = e2.department_id
+);
 
--- -- CTE + JOIN
--- WITH avg_salary AS (
---     SELECT department_id,
---            AVG(salary) AS avg_sal
---     FROM employees
---     GROUP BY department_id
--- )
--- SELECT e.name,
---        e.salary
--- FROM employees e
--- JOIN avg_salary a
--- ON e.department_id = a.department_id
--- WHERE e.salary > a.avg_sal;
+-- CTE + JOIN
+WITH avg_salary AS (
+    SELECT department_id,
+           AVG(salary) AS avg_sal
+    FROM employees
+    GROUP BY department_id
+)
+SELECT e.name,
+       e.salary
+FROM employees e
+JOIN avg_salary a
+ON e.department_id = a.department_id
+WHERE e.salary > a.avg_sal;
 
--- -- Q4. Find customer(s) who placed the highest number of orders.
--- WITH order_count AS (
---     SELECT customer_id,
---            COUNT(*) AS total_orders
---     FROM orders
---     GROUP BY customer_id
--- )
--- SELECT c.name,
---        oc.total_orders
--- FROM customers c
--- JOIN order_count oc
--- ON c.customer_id = oc.customer_id
--- WHERE oc.total_orders = (
---     SELECT MAX(total_orders)
---     FROM order_count
--- );
+-- Q4. Find customer(s) who placed the highest number of orders.
+WITH order_count AS (
+    SELECT customer_id,
+           COUNT(*) AS total_orders
+    FROM orders
+    GROUP BY customer_id
+)
+SELECT c.name,
+       oc.total_orders
+FROM customers c
+JOIN order_count oc
+ON c.customer_id = oc.customer_id
+WHERE oc.total_orders = (
+    SELECT MAX(total_orders)
+    FROM order_count
+);
 
--- -- Q5. Find employees earning the highest salary in each department.
--- -- Window Function
+-- Q5. Find employees earning the highest salary in each department.
+-- Window Function
 
--- WITH hs AS (
---     SELECT name,
---            salary,
---            department_id,
---            DENSE_RANK() OVER (
---                PARTITION BY department_id
---                ORDER BY salary DESC
---            ) AS hm
---     FROM employees
--- )
--- SELECT name,
---        salary,
---        department_id
--- FROM hs
--- WHERE hm = 1;
+WITH hs AS (
+    SELECT name,
+           salary,
+           department_id,
+           DENSE_RANK() OVER (
+               PARTITION BY department_id
+               ORDER BY salary DESC
+           ) AS hm
+    FROM employees
+)
+SELECT name,
+       salary,
+       department_id
+FROM hs
+WHERE hm = 1;
 
--- -- Correlated Subquery
--- SELECT name,
---        salary
--- FROM employees e
--- WHERE salary = (
---     SELECT MAX(salary)
---     FROM employees e2
---     WHERE e.department_id = e2.department_id
--- );
+-- Correlated Subquery
+SELECT name,
+       salary
+FROM employees e
+WHERE salary = (
+    SELECT MAX(salary)
+    FROM employees e2
+    WHERE e.department_id = e2.department_id
+);
 
--- Q6. Find customers who placed more than one order.
--- -- Using HAVING
--- SELECT c.name,
---        COUNT(*) AS total_orders
--- FROM customers c
--- JOIN orders o
--- ON c.customer_id = o.customer_id
--- GROUP BY c.customer_id, c.name
--- HAVING COUNT(*) > 1;
+Q6. Find customers who placed more than one order.
+-- Using HAVING
+SELECT c.name,
+       COUNT(*) AS total_orders
+FROM customers c
+JOIN orders o
+ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.name
+HAVING COUNT(*) > 1;
 
--- -- Using CTE
--- WITH hm AS (
---     SELECT customer_id,
---            COUNT(*) AS total_orders
---     FROM orders
---     GROUP BY customer_id
--- )
--- SELECT c.name,
---        h.total_orders
--- FROM customers c
--- JOIN hm h
--- ON c.customer_id = h.customer_id
--- WHERE h.total_orders > 1;
+-- Using CTE
+WITH hm AS (
+    SELECT customer_id,
+           COUNT(*) AS total_orders
+    FROM orders
+    GROUP BY customer_id
+)
+SELECT c.name,
+       h.total_orders
+FROM customers c
+JOIN hm h
+ON c.customer_id = h.customer_id
+WHERE h.total_orders > 1;
 
--- -- Q7. Find the second highest salary in each department.
+-- Q7. Find the second highest salary in each department.
 
--- -- Window Function
--- WITH ranked AS (
---     SELECT department_id,
---            name,
---            salary,
---            DENSE_RANK() OVER (
---                PARTITION BY department_id
---                ORDER BY salary DESC
---            ) AS rn
---     FROM employees
--- )
--- SELECT department_id,
---        name,
---        salary
--- FROM ranked
--- WHERE rn = 2;
+-- Window Function
+WITH ranked AS (
+    SELECT department_id,
+           name,
+           salary,
+           DENSE_RANK() OVER (
+               PARTITION BY department_id
+               ORDER BY salary DESC
+           ) AS rn
+    FROM employees
+)
+SELECT department_id,
+       name,
+       salary
+FROM ranked
+WHERE rn = 2;
 
--- --Alternative (Without Window Functions)
--- SELECT department_id,
---        name,
---        salary
--- FROM employees e
--- WHERE salary = (
---     SELECT MAX(salary)
---     FROM employees e2
---     WHERE e.department_id = e2.department_id
---       AND salary < (
---           SELECT MAX(salary)
---           FROM employees e3
---           WHERE e.department_id = e3.department_id
---       )
--- );
+--Alternative (Without Window Functions)
+SELECT department_id,
+       name,
+       salary
+FROM employees e
+WHERE salary = (
+    SELECT MAX(salary)
+    FROM employees e2
+    WHERE e.department_id = e2.department_id
+      AND salary < (
+          SELECT MAX(salary)
+          FROM employees e3
+          WHERE e.department_id = e3.department_id
+      )
+);
 
--- -- Q8. Find employees who have the same salary as at least one other employee.
--- SELECT name,
---        salary
--- FROM employees
--- WHERE salary IN (
---     SELECT salary
---     FROM employees
---     GROUP BY salary
---     HAVING COUNT(*) > 1
--- );
+-- Q8. Find employees who have the same salary as at least one other employee.
+SELECT name,
+       salary
+FROM employees
+WHERE salary IN (
+    SELECT salary
+    FROM employees
+    GROUP BY salary
+    HAVING COUNT(*) > 1
+);
 
--- -- Q9. Find the latest hired employee from each department.
--- WITH ranked AS (
---     SELECT department_id,
---            name,
---            hire_date,
---            ROW_NUMBER() OVER (
---                PARTITION BY department_id
---                ORDER BY hire_date DESC
---            ) AS rn
---     FROM employees
--- )
--- SELECT department_id,
---        name,
---        hire_date
--- FROM ranked
--- WHERE rn = 1;
+-- Q9. Find the latest hired employee from each department.
+WITH ranked AS (
+    SELECT department_id,
+           name,
+           hire_date,
+           ROW_NUMBER() OVER (
+               PARTITION BY department_id
+               ORDER BY hire_date DESC
+           ) AS rn
+    FROM employees
+)
+SELECT department_id,
+       name,
+       hire_date
+FROM ranked
+WHERE rn = 1;
 
--- -- Q10. Find the latest order placed by each customer.
--- WITH ranked AS (
---     SELECT order_id,
---            customer_id,
---            order_date,
---            amount,
---            ROW_NUMBER() OVER (
---                PARTITION BY customer_id
---                ORDER BY order_date DESC
---            ) AS rn
---     FROM orders
--- )
--- SELECT customer_id,
---        order_id,
---        order_date,
---        amount
--- FROM ranked
--- WHERE rn = 1;
+-- Q10. Find the latest order placed by each customer.
+WITH ranked AS (
+    SELECT order_id,
+           customer_id,
+           order_date,
+           amount,
+           ROW_NUMBER() OVER (
+               PARTITION BY customer_id
+               ORDER BY order_date DESC
+           ) AS rn
+    FROM orders
+)
+SELECT customer_id,
+       order_id,
+       order_date,
+       amount
+FROM ranked
+WHERE rn = 1;
 
 -- Q11 – Customers Who Ordered Every Product
 
@@ -305,3 +305,37 @@ JOIN salaries s
 ON e.department = s.department
 AND e.salary = s.max_sal ;
 
+-- Q.15 Find the customer(s) whose total order amount is the highest.
+
+-- METHOD - 1
+WITH max_sal AS (SELECT customer_id,
+        SUM(amount) AS total_amount
+FROM orders 
+GROUP BY customer_id 
+)
+
+SELECT customer_id ,
+        total_amount
+FROM max_sal
+WHERE total_amount = (
+    SELECT MAX(total_amount) FROM max_sal
+    ) ;
+
+-- METHOD - 2
+WITH max_sal AS (SELECT customer_id,
+        SUM(amount) AS total_amount
+FROM orders 
+GROUP BY customer_id 
+),
+
+ranked AS (SELECT customer_id,
+        total_amount,
+        DENSE_RANK() OVER(
+            ORDER BY total_amount DESC
+         ) AS rankings
+FROM max_sal 
+)
+
+SELECT customer_id,total_amount
+FROM ranked 
+WHERE rankings = 1 ;
